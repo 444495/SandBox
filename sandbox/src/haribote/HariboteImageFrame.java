4c4e608d76be8dab45440cf10f04f05dc64e7c46
444495 <easycomeseasybooooom@icloud.com>
2016-06-09 06:34:04 +0900

    あとグラフの読み込み機能をつければハリボテ完成

--- sandbox/src/haribote/HariboteImageFrame.java
+++ sandbox/src/haribote/HariboteImageFrame.java
@@ -9,6 +9,8 @@ import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JToolBar;
+import javax.swing.JButton;
 
 public class HariboteImageFrame extends JInternalFrame {
 	private static Point initializePosition = new Point(0,0);
@@ -17,22 +19,25 @@ public class HariboteImageFrame extends JInternalFrame {
 	 * Create the frame.
 	 */
 	public HariboteImageFrame(Image image) {
-		setLayout(new BorderLayout(0,0));
+		getContentPane().setLayout(new BorderLayout(0,0));
 		setResizable(true);
 		setClosable(true);
 		setMaximizable(true);
 		setIconifiable(true);
 		JPanel contentPane = new JPanel();
 		setContentPane(contentPane);
-		contentPane.setLayout(new GridLayout(2,2));
+		contentPane.setLayout(new BorderLayout(0,0));
+		JPanel bodyPanel = new JPanel();
+		bodyPanel.setLayout(new GridLayout(2,2));
+		contentPane.add(bodyPanel, BorderLayout.CENTER);
 		JScrollPane scrollPane1 = new JScrollPane();
 		JScrollPane scrollPane2 = new JScrollPane();
 		JScrollPane scrollPane3 = new JScrollPane();
 		JScrollPane scrollPane4 = new JScrollPane();
-		contentPane.add(scrollPane1);
-		contentPane.add(scrollPane2);
-		contentPane.add(scrollPane3);
-		contentPane.add(scrollPane4);
+		bodyPanel.add(scrollPane1);
+		bodyPanel.add(scrollPane2);
+		bodyPanel.add(scrollPane3);
+		bodyPanel.add(scrollPane4);
 		JPanel panel1 = new JPanel();
 		panel1.add(new JLabel(new ImageIcon(image)));
 		scrollPane1.setViewportView(panel1);
@@ -45,6 +50,12 @@ public class HariboteImageFrame extends JInternalFrame {
 		JPanel panel4 = new JPanel();
 		panel4.add(new JLabel(new ImageIcon(image)));
 		scrollPane4.setViewportView(panel4);
+		
+		JToolBar toolBar = new JToolBar();
+		contentPane.add(toolBar, BorderLayout.NORTH);
+		
+		JButton saveButton = new JButton("Save");
+		toolBar.add(saveButton);
 		
 		setSize(image.getWidth(this)*2, image.getHeight(this)*2);
 		
